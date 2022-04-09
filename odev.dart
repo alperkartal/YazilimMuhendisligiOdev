cef64afea385db3114fc38a528f87db6e6d1cf1c
Alper Kartal <alperkartal20@gmail.com>
2022-04-09 22:28:06 +0300

    bolme hatasi düzeltidi

--- odev.dart
+++ odev.dart
@@ -2,6 +2,9 @@ int toplama(int a, int b) {
   return a + b;
 }
 
-double bolme(int a, int b) {
-  return a / b;
+bolme(int a, int b) {
+  if (b == 0)
+    return print("bölme işlemi yapılamaz.");
+  else
+    return a / b;
 }
